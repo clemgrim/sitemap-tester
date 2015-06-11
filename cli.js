81ea5908f7531d90c5913cadb59b351713f6802d
Clement Grimault <clemgrim@gmail.com>
2015-06-11 09:46:03 +0200

    Create cli file, organize main file, first build

---
+++ cli.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+var yargs = require('yargs');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var symbols = require('log-symbols');
+var chalk = require('chalk');
+var sitemap = require('./');
+var argv;
+
+argv = yargs.usage('Usage: $0 -u <url>')
+	.alias('u', 'url')
+	.alias('w', 'write')
+	.alias('h', 'help')
+	.alias('o', 'open')
+	.alias('v', 'version')
+	.describe('u', 'Url du site')
+	.describe('w', 'Ecrire les résultats')
+	.describe('o', 'Ouvre le fichier')
+	.boolean('o')
+	.boolean('w')
+	.version(function() {return require('./package').version;})
+	.argv;
+
+if (argv.help) {
+	yargs.showHelp();
+	process.exit(0);
+}
+
+sitemap.get(argv.url, function (err, data) {
+	if (err) {
+		die(err);
+	}
+	
+	console.log(chalk.green('>'), ' Sitemap analysé : ', chalk.cyan(data.urlset.url.length, ' urls à valider'));
+	
+	progress();
+	interval = setInterval(progress, 2000);
+	
+	sitemap.explore(data, function (e, stats) {
+		clear();
+		
+		if (e) {
+			die(e);
+		}
+		
+		console.log(symbols.success + ' Success: ', stats.success);
+		console.log(symbols.error + ' Fail: ', stats.fail);
+
+		if (stats.errors.length) {
+			console.log('\n');
+			
+			stats.errors.forEach(function (page) {
+				console.log(chalk.gray('[' + page.status + ']'), page.url);
+			});
+		}
+
+		if (argv.write) {
+			fs.writeFile('./sitemap.json', JSON.stringify(stats, null, 2), function (err) {
+				if (err) {
+					die(err);
+				}
+				
+				console.log('\n');
+				console.log(chalk.green('>>') + ' Fichier écrit ' + chalk.cyan('./sitemap.json'));
+				
+				if (argv.open) {
+					require('opn')('./sitemap.json');
+				}
+			});
+		}
+	});
+});
+
+function clear() {
+	clearInterval(interval);
+	console.log('\n');
+}
+
+function die(msg, status) {
+	msg = msg || 'Une erreur est survenue'
+	console.log(chalk.red(msg), status ? chalk.cyan(argv.url) : '');
+	process.exit(0);
+}
+
+function progress () {
+	process.stdout.write('.');
+}
--- index.js
+++ index.js
@@ -1,119 +1,88 @@
 var request = require('request');
 var _ = require('lodash');
-var chalk = require('chalk');
 var xml = require('xml2js');
-var symbols = require('log-symbols');
 var Q = require('q');
-var yargs = require('yargs');
-var url, interval, argv;
-var fs = require('fs');
-var exec = require('child_process').exec;
 var start = new Date();
 
-argv = yargs.usage('Usage: $0 -u <url>')
-			.alias('u', 'url')
-			.alias('w', 'write')
-			.alias('h', 'help')
-			.alias('o', 'open')
-			.alias('v', 'version')
-			.describe('u', 'Url du site')
-			.describe('w', 'Ecrire les résultats')
-			.describe('o', 'Ouvre le fichier')
-			.boolean('o')
-			.boolean('w')
-			.version(function() {return require('./package').version;})
-			.argv;
+module.exports.get = function (url, cb) {
+	cb = cb || _.noop;
 
-if (argv.help) {
-	yargs.showHelp();
-	process.exit(0);
-}
+	if (!url) {
+		cb('Veuillez fournir une url');
+		return;
+	}
 
-url = argv.url;
+	url = _.trimRight(url, '/') + '/sitemap.xml';
 
-if (!url) {
-	die('Veuillez fournir une url');
-}
-
-url = _.trimRight(url, '/') + '/sitemap.xml';
-
-Q.nfcall(request.get, url)
-	.then(function (res) {
-		if (res[0].statusCode !== 200) {
-			throw new Error('Impossible d\'ouvrir le sitemap');
-		}
-
-		return Q.nfcall(xml.parseString, res[1]);
-	})
-	.then(function (data) {
-		if (!data.urlset || !_.isArray(data.urlset.url)) {
-			throw new Error ('Sitemap invalide');
-		}
-		
-		console.log(chalk.green('>'), ' Sitemap analysé : ', chalk.cyan(data.urlset.url.length, ' urls à valider'));
-		
-		progress();
-		interval = setInterval(progress, 2000);
-		
-		var promises = [];
-		
-		_.chunk(data.urlset.url, 5).forEach(function (group, i) {
-			group.forEach(function (entry, k) {
-				var url = entry.loc[0];
-				
-				var dfd = Q.defer();
-				
-				setTimeout(function () {
-					request.head(url, function (err, res, body) {
-						if (err) {
-							dfd.reject(err);
-						} else {
-							dfd.resolve(res);
-						}
-					});
-				}, i * 1000)
-				
-				promises.push(dfd.promise);
-			});
-		});
-		
-		return Q.all(promises);
-	})
-	.then(function (res) {
-		var yep = [], nope = [], dfd = Q.defer();
-		
-		res.forEach(function (r) {
-			var uri = r.request.uri.href;
-				
-			if (r.statusCode !== 200) {
-				nope.push({url: uri, status: r.statusCode});
-			} else {
-				yep.push({url: uri, status: r.statusCode});
+	Q.nfcall(request.get, url)
+		.then(function (res) {
+			if (res[0].statusCode !== 200) {
+				throw new Error('Impossible d\'ouvrir le sitemap');
 			}
-		});
-		
-		dfd.resolve({yep: yep, nope: nope, total: res.length});
-		
-		return dfd.promise;
-	})
-	.then(function (stats) {
-		clear();
-		
-		console.log(symbols.success + ' Success: ', stats.yep.length);
-		console.log(symbols.error + ' Fail: ', stats.nope.length);
-		
-		if (stats.nope.length) {
-			console.log('\n');
+	
+			return Q.nfcall(xml.parseString, res[1]);
+		})
+		.then(function (data) {
+			if (!data.urlset || !_.isArray(data.urlset.url)) {
+				throw new Error ('Sitemap invalide');
+			}
 			
-			stats.nope.forEach(function (page) {
-				console.log(chalk.gray('[' + page.status + ']'), page.url);
+			data.sitemapUrl = url;
+			
+			cb(null, data);
+		})
+		.fail(function (msg) {
+			cb(msg);
+		});
+};
+
+module.exports.explore = function (data, cb) {
+	cb = cb || _.noop;
+	
+	var promises = [];
+	
+	_.chunk(data.urlset.url, 5).forEach(function (group, i) {
+		group.forEach(function (entry, k) {
+			var url = entry.loc[0];
+			
+			var dfd = Q.defer();
+			
+			setTimeout(function () {
+				request.head(url, function (err, res, body) {
+					if (err) {
+						dfd.reject(err);
+					} else {
+						dfd.resolve(res);
+					}
+				});
+			}, i * 800)
+			
+			promises.push(dfd.promise);
+		});
+	});
+	
+	Q.all(promises)
+		.then(function (res) {
+			var yep = [], nope = [], dfd = Q.defer();
+			
+			res.forEach(function (r) {
+				var uri = r.request.uri.href;
+					
+				if (r.statusCode !== 200) {
+					nope.push({url: uri, status: r.statusCode});
+				} else {
+					yep.push({url: uri, status: r.statusCode});
+				}
 			});
-		}
-		
-		if (argv.write) {
+			
+			dfd.resolve({yep: yep, nope: nope, total: res.length, url: data.sitemapUrl});
+			
+			return dfd.promise;
+		})
+		.then(function (stats) {
 			var now = new Date();
 			var output = {
-              sitemap: url,
+	          sitemap: stats.url,
 			  date: start.toLocaleString(),
 			  time: (now.getTime() - start.getTime()) / 1000,
 			  pages: stats.total,
@@ -122,39 +91,9 @@ Q.nfcall(request.get, url)
 			  errors: stats.nope
 			};
 			
-			return Q.nfcall(fs.writeFile, 'sitemap.json', JSON.stringify(output, null, 2));
-		}
-		
-		return Q.promise(function (r) {
-			r(false);
+			cb(null, output);
 		})
-	})
-	.then(function (status) {
-		if (status !== false) {
-			console.log('\n');
-			console.log(chalk.green('>>') + ' Fichier écrit ' + chalk.cyan('./sitemap.json'));
-			
-			if (argv.open) {
-				require('opn')('./sitemap.json');
-			}
-		}
-	})
-	.fail(function (msg) {
-		clear();
-		die(msg, url);
-	});
-
-function clear() {
-	clearInterval(interval);
-	console.log('\n');
-}
-
-function die(msg, status) {
-	msg = msg || 'Une erreur est survenue'
-	console.log(chalk.red(msg), status ? chalk.cyan(url) : '');
-	process.exit(0);
-}
-
-function progress () {
-	process.stdout.write('.');
-}
+		.fail(function (msg) {
+			cb(msg);
+		});
+};
